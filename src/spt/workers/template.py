a61c186e063ac168c5d3dafc3c3f571eb13900eb
CVE <verdier1706@gmail.com>
2024-06-14 20:31:46 +0200

    fix wsproto missing

--- src/spt/workers/template.py
+++ src/spt/workers/template.py
@@ -1,17 +1,25 @@
 from spt.services.service import Worker, Service
 from spt.utils import create_temp_file, remove_temp_file, get_available_device
+from spt.models.workers import WorkerBaseRequest
+from pydantic import BaseModel
+from typing import Union, Dict, Any
 
 class Template(Worker):
     def __init__(self, name: str, service: Service, model: str, logger):
         super().__init__(name=name, service=service, model=model, logger=logger)
         self.my_model = None
 
-    async def work(self, request: BaseModel) -> BaseModel:
+    async def work(self, request: WorkerBaseRequest) -> BaseModel:
         await super().work(request)
 
         # load self.model ...
 
         return None
+
+    async def stream(self, data: Union[bytes | str | Dict[str, Any]]) -> Union[bytes | str | Dict[str, Any]]:
+        # do something with data
+
+        return data
 
     def cleanup(self):
         super().cleanup()
