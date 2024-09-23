c113418badf6c1067b98641c3c43c501f0aa4b4a
CVE <verdier1706@gmail.com>
2024-09-23 13:02:29 +0200

    tests files organisation

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #python -m pip freeze --exclude-editable > constraints.txt
 
-docker login -u verdier1706@gmail.com -p 71662F0e32 docker.io && \
+docker login -u verdier1706@gmail.com -p ${DOCKER_TOKEN} docker.io && \
 echo "Building docker image..." && \
-#docker build -t spt-smi:$(git rev-parse --short HEAD) --progress=plain --platform linux/amd64 -f ./docker-compose/Dockerfile . && \
 docker build -t spt-smi:$(git rev-parse --short HEAD) --platform linux/amd64 -f ./docker-compose/Dockerfile . && \
 echo "Pushing docker image..." && \
 docker tag spt-smi:$(git rev-parse --short HEAD) spongetheory/spt-smi:latest && \
 docker push spongetheory/spt-smi:latest && \
 echo "Deploying to portainer..." && \
-curl -X POST https://portainer.sponge-theory.dev/api/stacks/webhooks/5c447230-9a3f-44e9-af63-38bf187dee8a
+curl -X POST ${HOOK_URL}
